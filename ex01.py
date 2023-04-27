fda957c98b35462aa4c9cc456c3110c7cff44a41
André Oliveira <115578007+AndreSantos09@users.noreply.github.com>
2023-04-27 20:11:44 -0300

    a

---
+++ ex01.py
@@ -0,0 +1,14 @@
+nome = 'André'
+sobrenome = 'Santos'
+idade = 19
+ano_nascimento = 2023 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.89
+
+
+print('nome',nome)
+print('sobrenome',sobrenome)
+print('idade',idade)
+print('ano de nascimento',ano_nascimento)
+print('é maior de idade?',maior_de_idade)
+print('altura em metros',altura_metros)
