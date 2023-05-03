60a074e01141b8fa9019dbb41c6c9690b140277e
André Oliveira <115578007+AndreSantos09@users.noreply.github.com>
2023-05-03 19:59:56 -0300

    Update ex03.py

--- exercicios.py/ex03.py
+++ exercicios.py/ex03.py
@@ -3,3 +3,4 @@ altura = 1.87
 peso = 63
 imc =  peso/altura ** 2
 print(nome, 'tem', altura,' de altura e tem um IMC de :',imc)
+result = f'{nome} tem {altura} de altura e tem um IMC de : {imc:.4f}'
