4548eae3ac6006743b53034e195649c0fb2fcb09
André Oliveira <115578007+AndreSantos09@users.noreply.github.com>
2023-05-05 11:02:29 -0300

    Update ex06.py

--- exercicios.py/ex06.py
+++ exercicios.py/ex06.py
@@ -1,15 +1,27 @@
-numero_int = input('digite um numero inteiro ')
+#numero_int = input('digite um numero inteiro ')
 
-try:
-    numero_int = float(numero_int)
-    par_impar = numero_int % 2 == 0
-    par_impar_str = 'impar'
+#try:
+    # numero_int = float(numero_int)
+    # par_impar = numero_int % 2 == 0
+    # par_impar_str = 'impar'
     
 
-    if par_impar:
-        par_impar_str = 'par'
+    # if par_impar:
+        #par_impar_str = 'par'
         
-    print(f'o numero {numero_int} é {par_impar_str}')    
+    # print(f'o numero {numero_int} é {par_impar_str}')    
 
-except:
-    print(f'{numero_int} não é um numero inteiro')
+#except:
+    #print(f'{numero_int} não é um numero inteiro')
+    
+    
+num = input('numero ')
+
+if "." in num:
+    num = float(num) 
+    print(f'o número {num} não é inteiro')       
+else:
+    num = int (num)
+    print(f'o número {num} é um inteiro')
+    
+
